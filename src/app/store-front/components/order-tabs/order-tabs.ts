bb3e62c116712cd2c38f29b101e7cbb651a22cdf
rodrilang <rodrigolang90mail.com>
2025-10-28 00:10:31 -0300

    resuelvo conflictos

--- src/app/store-front/components/order-tabs/order-tabs.ts
+++ src/app/store-front/components/order-tabs/order-tabs.ts
@@ -1,6 +1,5 @@
 import { Component, computed, inject, signal } from '@angular/core';
 import { CartView } from "./cart-view/cart-view";
-import { TableOrders } from "./table-orders/table-orders";
 import { MyOrders } from "./my-orders/my-orders";
 import { CartService } from '../../services/cart.service';
 import { OrderService } from '../../services/order.service';
