7f9f068fa1cf0f5681f27a2a2651ca5ca5b8804c
rodrilang <rodrigolang90mail.com>
2025-12-03 20:11:11 -0300

    json config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,3 @@ export const environment = {
   production: true,
   baseUrl: 'http://localhost:8080/api/v1'
 };
-<<<<<<< HEAD
-
-basesUrl: 'https://api.dinno.com.ar/api/v1'
-=======
->>>>>>> origin/dev
